a6c4cba27550c9ec40bc8e3592493569b2b0938c
RohanBade <baderohan111@gmail.com>
2023-07-10 08:53:39 +0545

    Registered Transaction and some admin attributes modified

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from home.models import Transaction
+
+admin.site.site_header = "Enterprise Resource Planner(E.R.P.) Admin Panel"
+admin.site.site_title = "ERP Admin Portal"
+admin.site.index_title = "Welcome to E.R.P. Researcher Portal"
 
 # Register your models here.
+admin.site.register(Transaction)
